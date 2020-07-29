b1c2435364d54198f527382e50791ffa020c28df
JiangHao <978561120@qq.com>
2020-07-29 11:21:34 +0800

    underscore

---
+++ javascript/grammar/combination.js
@@ -0,0 +1,13 @@
+// 函数组合 fn3(fn2(fn1(fn0(x)))) 化为 fn3(fn2, fn1, fn0(x))
+
+
+function compose() {
+	var args = arguments
+	var start = args.length - 1
+	return function() {
+		var i = start
+		var result = args[start].apply(this, arguments)
+		while(i--) result = args[i].call(this, result)
+		return reuslt
+	}
+}
--- javascript/grammar/debounce.js
+++ javascript/grammar/debounce.js
@@ -1,14 +1,18 @@
 var count = 1
 var container = document.getElementById('container')
 
-function getUserAction() {
+function getUserAction(event) {
+	// console.log(this)
+	// console.log(e)
 	container.innerHTML = count++
 }
 
-container.onmousemove = debounce(getUserAction, 1000)
+// container.onmousemove = debounce4(getUserAction, 1000)
+
+container.onmousemove = debounce4(getUserAction, 1000, true)
 
 // 1
-function debounce(func, wait) {
+function debounce(func, wait) { // this 指向 window
 	var timeout
 
 	return function() {
@@ -18,8 +22,8 @@ function debounce(func, wait) {
 }
 
 // 2 解决1 中的this 指向
-function debounce2(func, wait) {
-	var tiemout
+function debounce2(func, wait) { // this 指向 func
+	var timeout
 
 	return function() {
 		var context = this
@@ -30,6 +34,72 @@ function debounce2(func, wait) {
 	}
 }
 
+// this 指向
+// event 对象
+function debounce3(func, wait) {
+	var timeout
+
+	return function() {
+		var context = this
+		var args = arguments
+
+		clearTimeout(timeout)
+		timeout = setTimeout(function() {
+			func.apply(context, args)
+		}, wait)
+	}
+}
+
+function debounce4(func, wait, immediate) { // 立刻执行
+	var timeout
+
+	return function() {
+		var context = this
+		var args = arguments
+		if (timeout) {
+			clearTimeout(timeout)
+		}
+
+		if (immediate) {
+			var callNow = !timeout
+			timeout = setTimeout(function() {
+				timeout = null
+			}, wait)
+			if (callNow) {
+				func.apply(context, args)
+			}
+		} else {
+			timeout = setTimeout(function() {
+				func.apply(context, args)
+			}, wait)
+		}
+	}
+}
+
+function debounce5(func, wait, immediate) {
+	var timeout, result
+	return function() {
+		var context = this
+		var args = arguments
+
+		if(timeout) clearTimeout(timeout)
+		if (immediate) {
+			var callNow = !timeout
+			timeout = setTimeout(function() {
+				timeout = null
+			}, wait)
+			if (callNow) {
+				result = func.apply(context, args)
+			}
+		} else {
+			timeout = setTimeout(function() {
+				func.apply(context, args)
+			}, wait)
+		}
+
+		return result
+	}
+}
 
 
 
