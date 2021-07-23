265423f7dcefff40106dd6a7c0538adab0609460
jianghao <jianghao@haplox.com>
2021-07-23 18:52:31 +0800

    css

---
+++ css/index.css
@@ -0,0 +1,29 @@
+a:link {color:  #000000;}
+a:link {color:#000000;}      /* 未访问链接*/
+a:visited {color:#00FF00;}  /* 已访问链接 */
+a:hover {color:#FF00FF;}  /* 鼠标移动到链接上 */
+a:active {color:#0000FF;}  /* 鼠标点击时 */
+a:link {text-decoration:none;}    /* unvisited link */
+a:visited {text-decoration:none;} /* visited link */
+a:hover {text-decoration:underline;}   /* mouse over link */
+a:active {text-decoration:underline;}  /* selected link */
+a:link {background-color:#B2FF99;}    /* 未访问链接 */
+a:visited {background-color:#FFFF85;} /* 已访问链接 */
+a:hover {background-color:#FF704D;}   /* 鼠标移动到链接上 */
+a:active {background-color:#FF704D;}  /* 鼠标点击时 */
+
+/* // 背景 */
+div.card {
+  width: 250px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: center;
+}
+div.header {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px;
+    font-size: 40px;
+}
+div.container {
+    padding: 10px;
+}
