afe9a1f1632b8f889f8bd7dd94d23cf4eac9828b
jianghao <978561120@qq.com>
2021-10-31 22:42:33 -0400

    rust c

--- c++/index.cpp
+++ c++/index.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 // main() 是程序开始执行的地方
 
-int main()// Wed Dec 21 09:52:14 +0100 RST 2011
-{    // calculating times:
+int main()                // Wed Dec 21 09:52:14 +0100 RST 2011
+{                         // calculating times:
    cout << "Hello World"; // 输出 Hello World
    return 0;
 }
