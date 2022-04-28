c31fa52192cddb323d3217769159a9c3e22258d1
jianghao <jianghao@haplox.com>
2022-04-28 20:39:40 +0800

    fix: go

---
+++ go/for/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type App struct {
+	AppAccount []*AppAccount
+}
+
+type AppAccount struct {
+	AppGroup []*AppGroup
+}
+
+type AppGroup struct {
+	Name string
+}
+
+func main() {
+	app := new(App)
+	account := new(AppAccount)
+	group := new(AppGroup)
+
+	account.AppGroup = append(account.AppGroup, group, group, group)
+	app.AppAccount = append(app.AppAccount, account, account, account)
+
+	x := 0
+	for _, appAccount := range app.AppAccount {
+		for _, appGroup := range appAccount.AppGroup {
+			appGroup.Name = fmt.Sprintf("--> %v", x)
+			x++
+		}
+	}
+
+	for _, appAccount := range app.AppAccount {
+		for _, appGroup := range appAccount.AppGroup {
+			fmt.Println(appGroup.Name)
+		}
+	}
+
+}
